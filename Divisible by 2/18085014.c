de11891518c9fe5eff04302a61ae2fa52db9291a
ayush922000 <44476700+ayush922000@users.noreply.github.com>
2018-10-25 23:47:00 +0530

    Create 18085014.c

---
+++ Divisible by 2/18085014.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{int a,b;
+printf(" enter any number a ");
+scanf("%d",&a);
+b=a%2;
+if(b==0)
+printf("number is even");
+else
+printf("number is odd");
+return 0;
+}
+
